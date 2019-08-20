4f5685f9b5676b9732d490d071e0f75822e009a8
chandan sharma <sharmachandan351@gmail.com>
2019-08-20 21:18:57 +0545

    handleChange and handleSubmit functions are added

--- src/pages/Recipes.js
+++ src/pages/Recipes.js
@@ -1,9 +1,32 @@
 import React,{Component} from 'react';
-
+import Search from '../component/Search';
+import RecipiList from '../component/RecipeList';
 export default class Recipes extends Component{
+  constructor(props){
+    super(props)
+  }
+  state={
+    recipies:[{
+      id:1,
+      title:"hello",
+      img:""
+    }],
+    search:''
+  }
+  handleChange=e=>{
+    this.setState({
+      search:e.target.value
+    })
+  }
+  handleSubmit=e=>{
+    e.preventDefault()
+  }
   render(){
     return(
-      <h1>Me from Receipies</h1>
+      <>
+        <Search search={this.state.search} handleChange={this.handleChange} handleSubmit={this.handleSubmit}/>
+        <RecipiList recipies={this.state.recipies}/>
+      </>
     );
   }
 }
