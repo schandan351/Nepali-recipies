27c9cf7160b2f79a3081d3bbf3b43033f3ef697b
chandan sharma <sharmachandan351@gmail.com>
2019-08-20 19:50:02 +0545

    default component added

--- src/pages/Default.js
+++ src/pages/Default.js
@@ -1,9 +1,13 @@
 import React,{Component} from 'react';
-
+import Header from "../component/Header";
+import {Link} from 'react-router-dom';
 export default class Default extends Component{
   render(){
     return(
-      <h1>Me from Default</h1>
+      <Header title="Error 404" styleClass="default-style">
+        <h1>OPPS WRONG LINKS!!</h1>
+        <Link to="/" className="mt-3 btn btn-primary btn-lg">Return Home</Link>
+      </Header>
     );
   }
 }
